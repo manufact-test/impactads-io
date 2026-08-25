5c33ebf6d8dc3ca5dd39e3390339e4dc9688f8c9
manufact-test <manufact.test@gmail.com>
2026-08-25 17:51:06 +0300

    Keep blog localization source-driven

--- wp-content/plugins/impact-accs-blog/includes/class-integration.php
+++ wp-content/plugins/impact-accs-blog/includes/class-integration.php
@@ -34,23 +34,6 @@ class IAB_Integration {
 	}
 
 	/**
-	 * Remove the retired public brand name from rendered blog markup.
-	 *
-	 * This runs server-side on the final blog HTML, so both the static chrome
-	 * template and dynamically injected post/card markup stay consistent.
-	 *
-	 * @param string $html Rendered blog HTML.
-	 * @return string
-	 */
-	private static function replace_legacy_brand( $html ) {
-		if ( ! is_string( $html ) || '' === $html ) {
-			return $html;
-		}
-
-		return str_replace( 'impact.accs', 'impact.', $html );
-	}
-
-	/**
 	 * @param string $html Empty default.
 	 * @param string $slug Post slug.
 	 * @return string
@@ -63,7 +46,7 @@ class IAB_Integration {
 			return '';
 		}
 
-		return self::replace_legacy_brand( IAB_Render::single( $post ) );
+		return IAB_Render::single( $post );
 	}
 
 	/**
@@ -71,6 +54,6 @@ class IAB_Integration {
 	 * @return string
 	 */
 	public static function inject_blog_index( $html ) {
-		return self::replace_legacy_brand( IAB_Render::inject_index( $html ) );
+		return IAB_Render::inject_index( $html );
 	}
 }
